bd42b77f9c1bcbae7c8becb24b39038c60e2e8cd
Peter Desnoyers <pjd@ccs.neu.edu>
2017-02-14 11:02:08 -0500

    final spanning tree tests

--- test-st.py
+++ test-st.py
@@ -51,7 +51,6 @@ def hexdump(pkt):
 
 # listen on all sockets
 def _listen_thread():
-    global listeners
     while True:
         rfds,ign1,ign2 = select.select([x.sock for x in listeners], [], [], 0.1)
         for s in rfds:
@@ -208,6 +207,9 @@ def test1():
     return val
 
 def print_error(pkt, p):
+    if not p:
+        print 'no packet received'
+        return
     for _p,q,msg in ((p,pkt,'received'), (pkt,p,'expected')):
         for i in range(0, len(_p), 16):
             print msg, ':', ' '.join(['%02x' % ord(x) for x in _p[i:i+16]])
@@ -292,7 +294,8 @@ def test3():
         #          ^^^^^^^ msg_age - bytes 44 and 45
         if rcvd[0:44] != xpctd[0:44] or rcvd[46:] != xpctd[46:]:
             print 'port', i, ': received bad BPDU:'
-            print_error(xpctd, rcvd)            
+            print_error(xpctd, rcvd) 
+            val = False
 
     print '  remote root: 01:01:01:01:01:01'
     # now a better root at cost 30, on port 1
@@ -305,6 +308,171 @@ def test3():
 
     for i,l in ((0,l0),(1,l1),(2,l2)):
         rcvd = l.pkts()[-1]
+        # root=02:.. cost=10 bridge_id = b_id port=<i> age=2.0
+        xpctd = ('\x01\x80\xc2\x00\x00\x00' + b_id + '\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+                 '\x01\x01\x01\x01\x01\x01\x00\x00\x00\x28\x80\x00' + b_id + '\x00' + chr(i) + 
+                 '\x0a\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        #          ^^^^^^^ msg_age - bytes 44 and 45
+        if rcvd[0:44] != xpctd[0:44] or rcvd[46:] != xpctd[46:]:
+            print 'port', i, ': received bad BPDU:'
+            print_error(xpctd, rcvd) 
+            val = False
+            
+    print '  better path to: 01:01:01:01:01:01'
+    # same root at cost 20, on port 2
+    # root ID=01:01..., cost=20, bridge ID=05:05..., age=xxx
+    pkt = ('\x01\x80\xc2\x00\x00\x00\x05\x05\x05\x05\x05\x05\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+           '\x01\x01\x01\x01\x01\x01\x00\x00\x00\x14\x80\x00\x05\x05\x05\x05\x05\x05\x00\x00' +
+           '\x07\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    w0.send(pkt)
+    time.sleep(4)
+
+    for i,l in ((0,l0),(1,l1),(2,l2)):
+        rcvd = l.pkts()[-1]
+        # root=01:.. cost=30 bridge_id = b_id port=<i> age=xxx
+        xpctd = ('\x01\x80\xc2\x00\x00\x00' + b_id + '\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+                 '\x01\x01\x01\x01\x01\x01\x00\x00\x00\x1e\x80\x00' + b_id + '\x00' + chr(i) + 
+                 '\x08\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        #          ^^^^^^^ msg_age - bytes 44 and 45
+        if rcvd[0:44] != xpctd[0:44] or rcvd[46:] != xpctd[46:]:
+            print 'port', i, ': received bad BPDU:'
+            print_error(xpctd, rcvd) 
+            val = False
+
+    stop_bridge(b)
+    l0.done(); l1.done(); l2.done()
+
+    print 'TEST 3:', 'passed' if val else 'FAILED'
+    return val
+
+#------------------------------------
+
+def timed_pkt(l, pkt):
+    l.received.append((pkt, time.time()))
+
+def test4():
+    global w0, w1, w2
+    val = True
+
+    print 'Test 4 - msg_age handling'
+
+    l0,l1,l2 = listener(w0,timed_pkt), listener(w1,timed_pkt), listener(w2,timed_pkt)
+    b_id = ''.join([chr(x) for x in [8] + 
+                    [random.randint(0, 255) for i in range(5)]])
+    
+    b = start_bridge(ether_ntoa(b_id))
+    time.sleep(4)
+
+    # flush received list
+    for l in (l0,l1,l2):
+        l.pkts()    
+
+
+    # rcv BPDU with msg_age=0, test that outgoing msg_age is 1 + time since reception
+    # root ID=02:02..., cost=0, bridge ID=02:..., age=0
+    pkt = ('\x01\x80\xc2\x00\x00\x00\x02\x02\x02\x02\x02\x02\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+           '\x02\x02\x02\x02\x02\x02\x00\x00\x00\x00\x80\x00\x02\x02\x02\x02\x02\x02\x00\x00' +
+           '\x00\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    t0 = time.time()
+    w0.send(pkt)
+    time.sleep(8)
+
+    for i,l in ((0,l0),(1,l1),(2,l2)):
+        for pkt,t in l.pkts():
+            expected_time = (t-t0) + 1.0
+            pkt_time = ord(pkt[44]) + ord(pkt[45])/256.0
+
+            if abs(pkt_time - expected_time) > 1.5:
+                print 'ERROR: msg_age %.2f (not %d.0)' % (pkt_time, expected_time)
+                expected_pkt = (pkt[0:44] + chr(int(expected_time)) + '\0' + pkt[46:])
+                print_error(expected_pkt, pkt)
+                val = False
+
+
+    # now send root BPDU with msg_age=8, verify outgoing msg_age
+    # root ID=01:02..., cost=0, bridge ID=02:..., age=8.0
+    pkt = ('\x01\x80\xc2\x00\x00\x00\x02\x02\x02\x02\x02\x02\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+           '\x01\x02\x02\x02\x02\x02\x00\x00\x00\x00\x80\x00\x02\x02\x02\x02\x02\x02\x00\x00' +
+           '\x08\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    t0 = time.time()
+    w1.send(pkt)
+    time.sleep(0.1)                       # first get rid of any sort-of-simultaneous packets
+    for l in (l0,l1,l2):
+        l.pkts()    
+    
+    time.sleep(8)
+    
+    for i,l in ((0,l0),(1,l1),(2,l2)):
+        for pkt,t in l.pkts():
+            expected_time = (t-t0) + 9.0
+            pkt_time = ord(pkt[44]) + ord(pkt[45])/256.0
+
+            if abs(pkt_time - expected_time) > 1.5:
+                print 'ERROR: msg_age %.2f (not %d.0)' % (pkt_time, expected_time)
+                expected_pkt = (pkt[0:44] + chr(int(expected_time)) + '\0' + pkt[46:])
+                print_error(expected_pkt, pkt)
+                val = False
+
+    stop_bridge(b)
+    l0.done(); l1.done(); l2.done()
+
+    print 'TEST 4:', 'passed' if val else 'FAILED'
+    return val
+
+#------------------------------------
+
+def last(l):
+    return l[-1] if l else None
+
+def test5():
+    global w0, w1, w2
+    val = True
+
+    print 'Test 5 - time out root information'
+
+    l0,l1,l2 = listener(w0), listener(w1), listener(w2)
+    b_id = ''.join([chr(x) for x in [8] + [random.randint(0, 255) for i in range(5)]])
+    
+    b = start_bridge(ether_ntoa(b_id))
+    time.sleep(4)
+
+    print '  neighbor root: 02:02:02:02:02:02 age=10.0'
+    # first a neighbor that's better, on port 0
+    # root ID=02:02..., cost=0, bridge ID=02:..., age=0
+    pkt = ('\x01\x80\xc2\x00\x00\x00\x02\x02\x02\x02\x02\x02\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+           '\x02\x02\x02\x02\x02\x02\x00\x00\x00\x00\x80\x00\x02\x02\x02\x02\x02\x02\x00\x00' +
+           '\x0A\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    w0.send(pkt)
+
+    time.sleep(10)
+    for l in (l0,l1,l2):
+        l.pkts()                          # flush received list
+
+    time.sleep(4)
+    for i,l in ((0,l0),(1,l1),(2,l2)):
+        rcvd = last(l.pkts())
+        # root=02:.. cost=10 bridge_id = b_id port=<i> age=2.0
+        xpctd = ('\x01\x80\xc2\x00\x00\x00' + b_id + 
+                 '\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' + b_id + 
+                 '\x00\x00\x00\x00\x80\x00' + b_id + '\x00' + chr(i) + 
+                 '\x00\x00\x14\x00\x02\x00\x0f\x00' + '\0\0\0\0\0\0\0\0')
+
+        if rcvd != xpctd:
+            print 'port', i, ': received bad BPDU:'
+            print_error(xpctd, rcvd)            
+            val = False
+
+    print '  remote root: 01:01:01:01:01:01'
+    # now a better root at cost 30, on port 1
+    # root ID=01:01..., cost=30, bridge ID=04:04..., age=4.0
+    pkt = ('\x01\x80\xc2\x00\x00\x00\x04\x04\x04\x04\x04\x04\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+           '\x01\x01\x01\x01\x01\x01\x00\x00\x00\x1e\x80\x00\x04\x04\x04\x04\x04\x04\x00\x00' +
+           '\x08\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    w0.send(pkt)
+    time.sleep(4)
+
+    for i,l in ((0,l0),(1,l1),(2,l2)):
+        rcvd = last(l.pkts())
         # root=02:.. cost=10 bridge_id = b_id port=<i> age=2.0
         xpctd = ('\x01\x80\xc2\x00\x00\x00' + b_id + '\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
                  '\x01\x01\x01\x01\x01\x01\x00\x00\x00\x28\x80\x00' + b_id + '\x00' + chr(i) + 
@@ -337,7 +505,52 @@ def test3():
     stop_bridge(b)
     l0.done(); l1.done(); l2.done()
 
-    print 'TEST 3:', 'passed' if val else 'FAILED'
+    print 'TEST 5:', 'passed' if val else 'FAILED'
+    return val
+
+#------------------------------------
+
+def test6():
+    global w0, w1, w2
+    val = True
+
+    print 'Test 6 - blocked port test'
+
+    l0,l1,l2 = listener(w0, no_bpdus), listener(w1,no_bpdus), listener(w2,no_bpdus)
+    b_id = ''.join([chr(x) for x in [8] + [random.randint(0, 255) for i in range(5)]])
+    
+    bproc = start_bridge(ether_ntoa(b_id))
+
+    # root 02:02... cost 0 bridge 02:02...
+    bpdu2 = ('\x01\x80\xc2\x00\x00\x00\x02\x02\x02\x02\x02\x02\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+             '\x02\x02\x02\x02\x02\x02\x00\x00\x00\x00\x80\x00\x02\x02\x02\x02\x02\x02\x00\x00' +
+             '\x00\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    # root 02:02... cost 10 bridge 03:03...
+    bpdu3 = ('\x01\x80\xc2\x00\x00\x00\x02\x02\x02\x02\x02\x02\x00\x26\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00' +
+             '\x02\x02\x02\x02\x02\x02\x00\x00\x00\x0a\x80\x00\x03\x03\x03\x03\x03\x03\x00\x00' +
+             '\x02\x00\x14\x00\x02\x00\x0f\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+    print '  root on port 0, block port 1'
+    for i in range(20):
+        w0.send(bpdu2)
+        w1.send(bpdu3)
+        time.sleep(2)
+
+    payload = struct.pack('!H', 0x900) + ''.join([chr(random.randint(48,90)) for i in range(50)])
+    a,b,c,d,e,f,x,y,z = getaddrs(9)
+
+    w0.send(d + a + payload); time.sleep(0.05)
+    w1.send(e + b + payload); time.sleep(0.05)
+    w2.send(f + c + payload)
+    time.sleep(0.1)
+    val = val and validate_list(l0.pkts(), [f+c+payload], 'test 6.1 port 0')
+    val = val and validate_list(l1.pkts(), [], 'test 6.1 port 1')
+    val = val and validate_list(l2.pkts(), [d+a+payload], 'test 6.1 port 2')
+
+    stop_bridge(bproc)
+    l0.done(); l1.done(); l2.done()
+
+    print 'TEST 6:', 'passed' if val else 'FAILED'
     return val
 
 #------------------------------------
@@ -349,7 +562,7 @@ passed = []
 if args.tests:
     testlist = [globals()['test' + n] for n in args.tests[0].split(',')]
 else:
-    testlist = [test1,test2,test3]
+    testlist = [test1,test2,test3,test4,test5,test6]
 #try:
 if True:
     for t in testlist:
